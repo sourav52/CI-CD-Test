e882850db8bfc1044370063ab293d186d3a8dce4
Sourav Dash <sourav.dash@amdocs.com>
2020-11-19 12:12:50 +0530

    Adding Failled test cases for testing

--- test.py
+++ test.py
@@ -8,6 +8,9 @@ class TestSum(unittest.TestCase):
     def test_sum_tuple(self):
         self.assertEqual(sum((1, 2, 3)), 6, "Should be 6")
 
+    def test_sum_tuple(self):
+        self.assertEqual(sum((1, 2, 4)), 6, "Should be 6")
+
 
 if __name__ == "__main__":
     unittest.main()
